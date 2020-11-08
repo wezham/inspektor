aaf6bae9715902a8df30e1e375d76170e9aa8b7c
wezham <wezham1@gmail.com>
2020-11-08 23:02:50 +0000

    honeypot ready for deployment

--- inspektor/main.py
+++ inspektor/main.py
@@ -6,19 +6,18 @@ from flask import request
 from inspektor import app
 
 IP_DIR = os.environ.get("IP_DIR")
+IP_FILES = os.path.join(IP_DIR, "ips.jsonl")
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def index(path):
-    ip_files = os.path.join(IP_DIR, "ips.jsonl")
-
     honey = {request.remote_addr: {k: v for k, v in request.headers}}
     date = datetime.now().strftime("%Y-%m-%d")
     honey[request.remote_addr].update({"date": date})
     honey[request.remote_addr]["args"] = {**request.args}
     honey[request.remote_addr]["path"] = path
-    with jsonlines.open(ip_files, mode="a") as ips:
+    with jsonlines.open(IP_FILES, mode="a") as ips:
         ips.write(honey)
 
     return f"<h1>Welcome - TBC!</h1>"
