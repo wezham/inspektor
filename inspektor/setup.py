4a718ca45c0542a4c6f0219a88e1dd8f75411a94
wezham <wezham1@gmail.com>
2020-11-10 21:17:28 +0000

    monorepo setup lad

--- inspektor/setup.py
+++ inspektor/setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="inspektor",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
 )
